005faf9e39a534798ef2408aaaf947b308053f14
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-01 00:57:53 +0900

    Apply route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Career from './pages/Career';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/projects'} element={<Projects />} />
+          <Route path={'/career'} element={<Career />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Career() {
-  return <div></div>;
+  return <div>career</div>;
 }
 
 export default Career;
