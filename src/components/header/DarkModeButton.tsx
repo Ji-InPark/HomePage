b8cace6b09adfcd004687676e50f0ccea4379ad2
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-01 14:30:09 +0900

    Remove useless variable

--- src/components/header/DarkModeButton.tsx
+++ src/components/header/DarkModeButton.tsx
@@ -4,18 +4,15 @@ import { darkModeAtoms } from '../../recoil/atoms';
 import Moon from '../../assets/icons/Moon';
 import { css } from '@emotion/react';
 import Sun from '../../assets/icons/Sun';
-import Colors from '../../assets/colors/Colors';
 
 function DarkModeButton() {
   const [darkMode, setDarkMode] = useRecoilState(darkModeAtoms);
-  const colors = Colors();
   const iconSize = 40;
 
   return (
     <span
       onClick={() => setDarkMode((prev) => !prev)}
       css={css({
-        color: colors.fontColor,
         '&:hover': {
           cursor: 'pointer',
         },
