3325de77bc198f3b08ddd6a2543343f72d16e44f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-01 23:04:48 +0900

    Update ShadowedText.tsx

--- src/components/text/ShadowedText.tsx
+++ src/components/text/ShadowedText.tsx
@@ -16,6 +16,7 @@ function ShadowedText({ text, fontSize }: Props) {
       css={css({
         fontSize,
         textShadow: useShadow ? '0px 4px 4px rgba(0, 0, 0, 0.25)' : 'none',
+        userSelect: 'none',
       })}
     >
       {text}
