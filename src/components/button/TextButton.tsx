c48620674e17acfa465697d5466a102182c139a0
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-01 21:26:17 +0900

    Delete useshadow prop

--- src/components/button/TextButton.tsx
+++ src/components/button/TextButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { Link } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { darkModeAtoms } from '../../recoil/atoms';
 
 interface Props {
   text: string;
@@ -8,6 +10,8 @@ interface Props {
 }
 
 function TextButton({ text, path }: Props) {
+  const useShadow = !useRecoilValue(darkModeAtoms);
+
   return (
     <span
       css={css({
@@ -16,7 +20,13 @@ function TextButton({ text, path }: Props) {
         userSelect: 'none',
       })}
     >
-      <Link css={css({ '&:visited': { color: 'inherit' } })} to={path}>
+      <Link
+        css={css({
+          textShadow: useShadow ? '0px 4px 4px rgba(0, 0, 0, 0.25)' : 'none',
+          '&:visited': { color: 'inherit' },
+        })}
+        to={path}
+      >
         {text}
       </Link>
     </span>
