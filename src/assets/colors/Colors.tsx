e535288fa2bfe7c12dc72e302c74072f653bab37
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-03 21:57:35 +0900

    Add basicBackgroud color

--- src/assets/colors/Colors.tsx
+++ src/assets/colors/Colors.tsx
@@ -8,6 +8,7 @@ function Colors() {
     homeBackground: darkMode
       ? 'linear-gradient(112.68deg, #000000 0%, rgba(0, 0, 0, 0.85) 50%, rgba(0, 0, 0, 0.6) 100%)'
       : 'linear-gradient(112.68deg, #86C8BC 0%, #FFFFFF 100%);',
+    basicBackground: darkMode ? '#000000' : '#86C8BC',
     fontColor: darkMode ? '#FFFFFF' : '#000000',
   };
 
