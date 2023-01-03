307f8bd180bbeb6acb4fff2a96590cfa30292d7c
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-03 21:57:53 +0900

    Add basic layout of Projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import Header from '../components/header/Header';
+import Colors from '../assets/colors/Colors';
 
 function Projects() {
-  return <div>projects</div>;
+  const colors = Colors();
+
+  return (
+    <div
+      css={css({
+        width: '100vw',
+        height: '100vh',
+        background: colors.basicBackground,
+      })}
+    >
+      <div
+        css={css({
+          width: '70%',
+          height: '100%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          color: colors.fontColor,
+        })}
+      >
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
