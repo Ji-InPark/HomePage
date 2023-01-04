5e4f418b2006b97d1665c11925cc352dead93ba2
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-04 22:29:51 +0900

    Create types about projects

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Project = {
+  imageUrl: string;
+  title: string;
+  subTitle: string;
+  links: Array<ProjectLink>;
+  description: string;
+};
+
+export type ProjectLink = {
+  icon: JSX.Element;
+  link: string;
+};
