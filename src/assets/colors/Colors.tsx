1b9b9e2bf701cb1e9863ba712472fd9e884a3573
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-04 21:26:23 +0900

    Replace static colors with map

--- src/assets/colors/Colors.tsx
+++ src/assets/colors/Colors.tsx
@@ -1,18 +1,17 @@
 import { useRecoilValue } from 'recoil';
 import { darkModeAtoms } from '../../recoil/atoms';
+import { COLORS } from './Index';
 
 function Colors() {
   const darkMode = useRecoilValue(darkModeAtoms);
 
-  const colors = {
+  return {
     homeBackground: darkMode
       ? 'linear-gradient(112.68deg, #000000 0%, rgba(0, 0, 0, 0.85) 50%, rgba(0, 0, 0, 0.6) 100%)'
-      : 'linear-gradient(112.68deg, #86C8BC 0%, #FFFFFF 100%);',
-    basicBackground: darkMode ? '#000000' : '#86C8BC',
-    fontColor: darkMode ? '#FFFFFF' : '#000000',
+      : `linear-gradient(112.68deg, ${COLORS.MINT} 0%, #FFFFFF 100%);`,
+    basicBackground: darkMode ? 'black' : COLORS.LIGHT_MINT,
+    fontColor: darkMode ? 'white' : 'black',
   };
-
-  return colors;
 }
 
 export default Colors;
